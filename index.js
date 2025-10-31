e109c7466b4bab6f0bd248a914c67e6e0fca613a
Arijit Chowdhuri <chowdhuriarijit@gmail.com>
2025-10-31 15:33:54 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,169 @@
+const EventEmitter = require("events");
+const { Server } = require("socket.io");
+
+const PORT = process.env.PORT || 8888;
+
+class PubSub extends EventEmitter {
+  constructor() {
+    super();
+    // Key is the subscriber ID and value is an object containing properties:
+    // channel (channel name), callback (listener) and timestamp (ISO string format)
+    this.subscribers = new Map();
+    // Key is the channel name and value is a boolean
+    this.channels = new Map();
+  }
+
+  /**
+   * Publishes message to a channel
+   *
+   * @param {string} channel Channel name
+   * @param {string} message Message
+   */
+  publish(channel, message) {
+    // TODO should emit channel and message.
+  }
+
+  /**
+   * Subscribe to a channel
+   *
+   * @param {string} channel Channel name
+   * @param {function} callback Callback function
+   * @returns {string}
+   */
+  subscribe(channel, callback) {
+    // TODO subscribe user to channel
+    // should:
+    // - add subscriber
+    // - add channel
+    // - Add listener to event, event name should be the channel name provided
+    // and the listener should be the callback provided 
+    // - return subscriber ID
+    const timeStampIso = (new Date()).toISOString();
+    const subscriberId = this.generateId()
+    this.subscribers.set(subscriberId, {channel, callback, timeStampIso})
+    this.channels.set(channel, true)
+    this.addListener(channel, callback)
+
+    return subscriberId;
+     
+  }
+
+  /**
+   * Unsubscribe from channel using subscriber ID
+   *
+   * @param {string} subscriberId Subscriber ID
+   * @returns {boolean}
+   */
+  unsubscribe(subscriberId) {
+    // TODO unsubscribe with subscriber ID
+    // should:
+    // - Remove listener from event
+    // - remove user from subscribers
+    // - return false on subscriber not found
+    // - return true on successful completion
+    console.log("subscriberId", subscriberId)
+    const subscriberObj = this.subscribers.get(subscriberId)
+    console.log(subscriberObj)
+    if (!subscriberObj) return false
+
+    const subscriberChannelName = subscriberObj.channel
+    const subscriberCallback = subscriberObj.callback
+    this.removeListener(subscriberChannelName, subscriberCallback)
+    return true
+  }
+
+  /**
+   * Returns a list of subscribers
+   * returns {string[]}
+   */
+  listSubscribers() {
+    // TODO return list of subscribers
+   
+    const subscribers = []
+    for (const [key, value] of this.subscribers) { // Using the default iterator (could be `map.entries()` instead)
+      console.log(`The value for key ${key} is ${value}`);
+    }
+
+  }
+
+  /**
+   * Returns a list of channels
+   * returns {string[]}
+   */
+  listChannels() {
+    // TODO return list of channels
+  }
+
+  /**
+   * Generates random ID
+   * returns {string}
+   */
+  generateId() {
+    // TODO generate a random ID
+    return Math.random().toString();
+  }
+}
+
+const io = new Server(PORT, {
+  cors: { origin: "http://127.0.0.1:8080" },
+});
+
+const ps = new PubSub();
+
+// On socket connection
+io.on("connection", (socket) => {
+  console.log("Connection established with client");
+
+  // example
+  socket.on("hello", (msg) => {
+    console.log(`User sent message: ${msg}`);
+    io.emit("message", "Hello to you too");
+  });
+
+  socket.on("publish", (channel, msg) => {
+    // TODO Publish message to channel
+    // no need to emit a socket message
+  });
+
+  // User sends a subscription request
+  socket.on("subscribe", (channel) => {
+
+
+    //io.emit("subscriber-id", {channel:"channel1", subscriberId: ps.generateId()})
+    // TODO 
+    // should: 
+    // - run the subscribe method on PubSub.
+    // - listener should emit a websocket message to channel 'message' and the messsage be '{channel name}: {message content}'
+    // - emit a websocket message to channel 'subscriber-id', and two arguments, channel name and subscriber id
+    const callBack = () =>{ socket.emit('message', channel + ': test message'); }
+    const subscriberId = ps.subscribe(channel, callBack)
+    if (subscriberId) {
+      socket.emit("subscriber-id", channel, subscriberId)
+    }
+  });
+
+  socket.on("unsubscribe", (subscriberId) => {
+    // TODO
+    // should:
+    // - run the unsubscribe method on PubSub
+    // - if successful emit a websocket messsage to channel 'unsubscribed', one argument: subscriber ID
+    const flag = ps.unsubscribe(subscriberId)
+    if (flag) {
+      socket.emit("unsubscribed", subscriberId)
+    }
+  });
+
+  socket.on("list-subscribers", () => {
+    // TODO
+    // should:
+    // - emit websocket message returning subscribers
+  });
+
+  socket.on("list-channels", () => {
+    // TODO
+    // should:
+    // - emit websocket message returning channels
+  });
+
+});
+
---
+++ test/index.test.js
@@ -0,0 +1,118 @@
+const { describe, test, expect } = require("@jest/globals");
+const { io } = require("socket.io-client");
+
+const PORT = process.env.PORT || 8888;
+
+const channelName1 = "channel1"
+const channelName2 = "channel2"
+const channelName3 = "channel3"
+
+let currentSubscriberId;
+
+describe("PubSub Tests", () => {
+  //jest.setTimeout(1000);
+  let socket;
+  beforeAll((done) => {
+    socket = io("ws://localhost:"+PORT);
+    socket.on("connect", () => {
+      done();
+    });
+  });
+
+  afterAll(() => {
+    if (socket.connected) {
+      socket.disconnect();
+    }
+  });
+
+  // This test is bugged out on previous candidate. Works on my computer.
+  // But this test is quite unnecessary and just to confirm connection basically.
+  // So I'm skipping it.
+  test.skip("Test example response", (done) => {
+    socket.emit("hello", "Hello Server")
+    socket.on("hello", response=> {
+      try {
+        expect(response).toBe("Hello to you too")
+        done()
+      } catch(e) {
+        done(e)
+      }
+    })
+  });
+
+  test("Subscribe", (done) => {
+    socket.emit("subscribe", channelName1)
+    socket.on("subscriber-id", (channel, subscriberId)=> {
+      try {
+        expect(channel).toBe(channelName1)
+        expect(typeof subscriberId).toBe("string")
+        socket.off("subscriber-id");
+        currentSubscriberId = subscriberId;
+        done()
+      } catch(e) {
+        done(e)
+      }
+    })
+  });
+
+  test("Unsubscribe", (done) => {
+    socket.emit("unsubscribe", currentSubscriberId)
+    socket.on("unsubscribed", (subscriberId)=> {
+      try {
+        expect(subscriberId).toBe(currentSubscriberId)
+        done()
+      } catch(e) {
+        done(e)
+      }
+    })
+  });
+
+  test.skip("Subscribe more", (done) => {
+    socket.emit("subscribe", channelName2)
+    socket.emit("subscribe", channelName3)
+    socket.on("subscriber-id", (_channel, subscriberId)=> {
+      try {
+        expect(typeof subscriberId).toBe("string")
+        currentSubscriberId = subscriberId;
+        done()
+      } catch(e) {
+        done(e)
+      }
+    })
+  });
+
+
+  test.skip("List subscribers", (done) => {
+    socket.emit("list-subscribers")
+    socket.on("list-subscribers", (subscribers)=> {
+      console.log("SUBSCRIBERS")
+      console.log(subscribers)
+      try {
+        expect(Array.isArray(subscribers)).toBe(true)
+        expect(subscribers.length).toBe(2)
+        done()
+      } catch(e) {
+        done(e)
+      }
+    })
+  });
+
+  test.skip("List channels", (done) => {
+    socket.emit("list-channels")
+    socket.on("list-channels", (channels)=> {
+      console.log("CHANNELS")
+      console.log(channels)
+      try {
+        expect(Array.isArray(channels)).toBe(true)
+        expect(channels.length).toBe(3)
+        expect(channels.includes(channelName1)).toBe(true)
+        expect(channels.includes(channelName2)).toBe(true)
+        expect(channels.includes(channelName3)).toBe(true)
+        done()
+      } catch(e) {
+        done(e)
+      }
+    })
+  });
+
+});
